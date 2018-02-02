d45f938a3743c0c88faf332dd3fe78de66321f49
Fabus <aleksandrfab@gmail.com>
2018-02-02 15:43:24 +0700

    02-02-2018 add Hibernate cache 2 level

--- src/main/java/ru/fab/model/Goods.java
+++ src/main/java/ru/fab/model/Goods.java
@@ -1,6 +1,14 @@
 package ru.fab.model;
 
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
 import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
 
 @NamedQueries({
         @NamedQuery(name = Goods.DELETE, query = "DELETE FROM Goods g WHERE g.id=:id"),
@@ -8,6 +16,7 @@ import javax.persistence.*;
 })
 @Entity
 @Table(name = "goods")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 public class Goods extends AbstractEntity{
 
     public static final String DELETE = "Goods.delete";
